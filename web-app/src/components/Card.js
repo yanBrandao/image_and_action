170d38d77d46f60cd5b600a39878a734b002f6a6
Yan Tapajós <ybrandao.d@gmail.com>
2020-11-13 04:32:56 -0300

    Added animation to card

--- web-app/src/components/Card.js
+++ web-app/src/components/Card.js
@@ -2,23 +2,64 @@ import React, {Component} from 'react';
 import './Card.css'
 import 'typeface-roboto'
 import backgroundCard from './image_action.png'
+import backgroundFront from './image_action_front.png'
 
 const cardStyle = {
     backgroundImage: `url(${backgroundCard})`
 };
 
+const cardFront = {
+    backgroundImage: `url(${backgroundFront})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: 'white'
+};
+
 class Card extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggle: false
+        };
+        this.rotate = this.rotate.bind(this);
+        this.card = React.createRef();
+    }
+
+    timeout(delay) {
+        return new Promise( res => setTimeout(res, delay) );
+    }
+    rotate() {
+        this.card.current.className = 'Card Rotate'
+        console.log(this.card.current.className)
+        setTimeout(function() { //Start the timer
+            this.card.current.className = 'Card FinishRotate' //After 1 second, set render to true
+            console.log(this.card.current.className)
+            this.setState(function(state) {
+                return {
+                    toggle: !state.toggle
+                };
+            });
+        }.bind(this), 500)
+
+    }
+
     render() {
+        const { toggle } = this.state;
+
         return (
-            <div className="Background" onMouseOver="rotateYDIV()">
-                <div className="Card" style={cardStyle}>
-                    <p className="Phrase">Carpinteiro</p>
-                    <p className="Phrase">Faca</p>
-                    <p className="Phrase">Calar</p>
-                    <p className="Phrase">Carburador</p>
-                    <p className="Phrase">Ping-Pong</p>
-                    <p className="Phrase">Quadrado</p>
+            <div className="Background">
+                <div className={'Card'}
+                     ref={this.card}
+                     onClick={this.rotate} style={toggle ? cardStyle : cardFront}>
+                    <p className="Phrase">{toggle ? 'Carpinteiro' : ''}</p>
+                    <p className="Phrase">{toggle ? 'Faca' : '' }</p>
+                    <p className="Phrase">{toggle ? 'Calar' : '' }</p>
+                    <p className="Phrase">{toggle ? 'Carburador' : '' }</p>
+                    <p className="Phrase">{toggle ? 'Ping-Pong' : '' }</p>
+                    <p className="Phrase">{toggle ? 'Quadrado' : '' }</p>
                 </div>
+
             </div>
         );
     }
